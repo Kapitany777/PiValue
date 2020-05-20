a19927d2f163f55c0c2d44d3ef5d1209a5851513
torokv@vnet.hu <Trk>
2020-05-20 11:09:38 +0200

    Adding Leibniz formula
    Some minor modifications

--- PiValue.dpr
+++ PiValue.dpr
@@ -6,7 +6,7 @@ program PiValue;
 uses
   System.SysUtils;
 
-function GetPiValue: double;
+function GetPiMonteCarlo: double;
 
 var
   total: integer;
@@ -15,28 +15,70 @@ var
   i: integer;
 
 begin
+  // https://academo.org/demos/estimating-pi-monte-carlo/
+
   total := 1000000;
   inner := 0;
 
   for i := 1 to total do
 
-    begin
-      x := 2 * Random() - 1;
-      y := 2 * Random() - 1;
-//      Writeln(x:5:3, ' ', y:5:3);
+  begin
+    x := 2 * Random() - 1;
+    y := 2 * Random() - 1;
+    // Writeln(x:5:3, ' ', y:5:3);
 
-      if (x * x + y * y <= 1) then
-        begin
-          inner := inner + 1;
-        end;
+    if (x * x + y * y <= 1) then
+    begin
+      inner := inner + 1;
     end;
+  end;
 
   Result := 4 * inner / total;
 end;
 
+function GetPiLeibniz: double;
+
+var
+  sum: double;
+  mult: double;
+  denominator: double;
+  i: Integer;
+
 begin
-  // Estimating Pi value using the Monte Carlo Method
+  // https://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80
+
+  sum := 0;
+  mult:=1;
+  denominator:=1;
+
+  for i := 1 to 1000000 do
+
+  begin
+    sum:=sum + mult * 1 / denominator;
+    mult:=mult * -1;
+    denominator:=denominator + 2;
+  end;
+
+  Result := 4 * sum;
+end;
+
+begin
+  // Calculate the estimated value of Pi
   Randomize;
-  Writeln('The estimated value of PI is : ', GetPiValue().ToString());
+
+  // Delphi Pi
+  Writeln('Delphi Pi: ', Pi.ToString());
+  Writeln;
+
+  // Estimating Pi value using the Monte Carlo Method
+  Writeln('Monte Carlo Method:');
+  Writeln('The estimated value of Pi is: ', GetPiMonteCarlo().ToString());
+  Writeln;
+
+  // Leibniz formula
+  Writeln('Leibniz formula:');
+  Writeln('The estimated value of Pi is: ', GetPiLeibniz().ToString());
+
   Readln;
+
 end.
